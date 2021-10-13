71e293025563438fdab0b612ff63f66be234aa22
am1rcek <xusandadajonov4385@gmail.com>
2021-10-13 03:30:45 -0700

    awd

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ let elModal = document.querySelector(".nav");
 let elLinethird =document.querySelector(".line2");
 let elLineOne =document.querySelector(".line1");
 let elLineThree =document.querySelector(".line3");
+let elLine = document.querySelectorAll('.experience__line');
+let elNavItem = document.querySelectorAll(".nav__item");
 
 elMenuBtn.addEventListener("click",function(){
     elModal.classList.toggle("grow-height");
@@ -10,6 +12,34 @@ elMenuBtn.addEventListener("click",function(){
     elLineOne.classList.toggle("rotate-line1")
     elLineThree.classList.toggle("rotate-line3")
 })
+
+
+elLine.forEach((item) => {
+  window.addEventListener("scroll",function(){
+    header = document.querySelector("header");
+    header.classList.add("header-bg",window.scrollY > 60);    
+    item.classList.toggle("float",window.scrollY > 300);
+    console.log(this.window.scrollY)
+  })
+})
+
+elNavItem.forEach((navLink) => {
+  window.addEventListener("scroll",function(){
+    if(this.window.scrollY == 0){
+      elNavItem[0].classList.add("nav__link--active");
+      elNavItem[1].classList.remove("nav__link--active");
+    }
+    else if(this.window.scrollY > 300){
+      elNavItem[0].classList.remove("nav__link--active",window.scrollY);
+      elNavItem[1].classList.add("nav__link--active",window.scrollY > 300);
+    }
+ 
+    console.log(this.window.scrollY)
+  })
+})
+
+
+
 
 var typed = new Typed('.name', {
   strings: [
@@ -20,3 +50,9 @@ var typed = new Typed('.name', {
     backSpeed: 90,
     loop: true
 });
+
+
+
+
+
+
