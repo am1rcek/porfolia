15c951747ae8847f3ec0987fb52dfd671ef91aee
am1rcek <xusandadajonov4385@gmail.com>
2022-02-23 21:53:45 +0500

    end

--- js/main.js
+++ js/main.js
@@ -6,35 +6,52 @@ let elLineThree =document.querySelector(".line3");
 let elLine = document.querySelectorAll('.experience__line');
 let elNavItem = document.querySelectorAll(".nav__item");
 
+
+
 elMenuBtn.addEventListener("click",function(){
     elModal.classList.toggle("grow-height");
     elLinethird.classList.toggle("remove-line");
-    elLineOne.classList.toggle("rotate-line1")
-    elLineThree.classList.toggle("rotate-line3")
+    elLineOne.classList.toggle("rotate-line1");
+    elLineThree.classList.toggle("rotate-line3");
 })
 
 
-elLine.forEach((item) => {
-  window.addEventListener("scroll",function(){
-    header = document.querySelector("header");
-    header.classList.toggle("header-bg",window.scrollY > 200);
-    item.classList.toggle("float",window.scrollY > 300);
-    console.log(this.window.scrollY)
-  })
-})
-
+window.addEventListener("scroll",function(){
+  header = document.querySelector("header");
+  header.classList.toggle("header-bg",window.scrollY > 200);
+  if(this.window.scrollY > 1429 && this.window.scrollY < 2933){
+    this.document.querySelector("body").classList.add("change-content")
+  }
+  else{
+    this.document.querySelector("body").classList.remove("change-content")
+  }
+});
 elNavItem.forEach((navLink) => {
   window.addEventListener("scroll",function(){
-    console.log(navLink)
-    if(this.window.scrollY > 500){
-      elNavItem[1].classList.add("nav__link--active");
-      elNavItem[0].classList.remove("nav__link--active");
-    }
-    else if(this.window.scrollY < 500){
+    if(this.window.scrollY > 0){
+      elNavItem.forEach(item => item.classList.remove("nav__link--active"))
       elNavItem[0].classList.add("nav__link--active");
-      elNavItem[1].classList.remove("nav__link--active");
-    };
-    console.log(this.window.scrollY)
+    }
+    if(this.window.scrollY > 410){
+      elNavItem.forEach(item => item.classList.remove("nav__link--active"))
+      elNavItem[1].classList.add("nav__link--active");
+    }
+    if(this.window.scrollY > 826){
+      elNavItem.forEach(item => item.classList.remove("nav__link--active"))
+      elNavItem[2].classList.add("nav__link--active");
+    }
+    if(this.window.scrollY > 1707){
+      elNavItem.forEach(item => item.classList.remove("nav__link--active"))
+      elNavItem[3].classList.add("nav__link--active");
+    }
+    if(this.window.scrollY > 2786){
+      elNavItem.forEach(item => item.classList.remove("nav__link--active"))
+      elNavItem[4].classList.add("nav__link--active");
+    }
+    if(this.window.scrollY > 3542){
+      elNavItem.forEach(item => item.classList.remove("nav__link--active"))
+      elNavItem[5].classList.add("nav__link--active");
+    }
   })
 })
 
@@ -44,7 +61,6 @@ elNavItem.forEach((navLink) => {
 var typed = new Typed('.name', {
   strings: [
     "Husan",
-    "HarDev",
     "Creative",
   ],
     typeSpeed: 80,
